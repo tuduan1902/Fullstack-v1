944aa28e9b612397fc37b66590a29898d2dba207
Tu Duan <uuxinhxinh2701@gmail.com>
2022-10-10 19:42:17 +0700

    create column of feed router on database

--- api/src/feed/feed.module.ts
+++ api/src/feed/feed.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { FeedService } from './services/feed.service';
 import { FeedController } from './controllers/feed.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { FeedPostEntity } from './models/post.entity';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([FeedPostEntity])
+  ],
   providers: [FeedService],
   controllers: [FeedController]
 })
